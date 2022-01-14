aaa44aadd33fd6a018de87bd4c8c53408e80de36
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-01-14 11:04:51 -0800

    add reset rules to inputs once submitted, fix form IDs

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,41 @@
+function resetForm() {
+  //
+  // when called, this function will reset the form
+  // all inputs and selects are set to blank
+  //
+
+  $("input#user-name").val("");
+  $("input#email").val("myemail@gmail.com");
+  $("select#leisure-activity").val("");
+  $("select#user-industry").val("");
+  $("select#chicken-or-egg").val("");
+  $("select#language-formatting").val("");
+  $("select#language-age").val("");
+  $("select#cutest-puppy").val("");
+  
+}
+
 $(document).ready(function () {
   $("form#answers").submit(function (event) {
     event.preventDefault();
 
-    $("#form-questions").hide();
-
+    $("#form-questions").hide(); // when [spin] is pushed, hide form
+    $("#form-results").toggle(); // and then show results div
     
-
-
-    $("#form-results").toggle();
-    
-
   });
 
+  // when email text box is clicked and myemail@gmail is still there, blank out
   $("input#email").click(function () {
-    $("input#email").val("");
+    if ($("input#email").val() === "myemail@gmail.com") {
+      $("input#email").val("");
+    }
   });
 
+  // when [spin again] is clicked, toggle div displays and reset the form
   $("#spin-again-button").click(function () {
     $("#form-questions").show();
     $("#form-results").toggle();
+    resetForm();
   });
 
 });
