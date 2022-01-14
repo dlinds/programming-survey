9d8b832237e0e4d1cedb62c2eb8fa13993cdd871
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-01-14 15:56:29 -0800

    further update logic to include more cases

--- js/scripts.js
+++ js/scripts.js
@@ -35,8 +35,8 @@ $(document).ready(function () {
     const langAge = $("select#language-age").val();
     const cutestPuppyName = $("select#cutest-puppy-name").val();
 
-    let languageResult;
-    let languageReasoning;
+    let languageResult = '';
+    let languageReasoning = '';
 
     $("#form-questions").hide(); // when [spin] is pushed, hide form
     $("#form-results").toggle(); // and then show results div
@@ -112,19 +112,64 @@ $(document).ready(function () {
           switch (leisureActivity) {
             case "Make Music": 
                   languageResult = "C++";
-                  languageReasoning = "Because you work in media and you like making music. Did you know that most audio plugins are made utilizing a C++ library called Juce?";
+                  languageReasoning = "Because you work in media and you like making music. Did you know that most audio plugins are made utilizing C++? There is even a library called Juce that can help jumpstart your journey!";
                   break; 
+            case "Video Games" :
+                  languageResult = "C++";
+                  languageReasoning = "You work in media and enjoy gaming! Unreal Engine is one of the standards for making video games. Unreal Engine happens to be build on top of C++!";
+                  break; 
+            case "Exercise" :
+                  languageResult = "PowerShell";
+                  languageReasoning = "You work in media and enjoy a bit of exercise, so PowerShell will be great for you as the method names are quite long - similar to the runs you are probably taking.";
+                  break;  
+            default:
+                  if (languageFormatting === "Precise") {
+                    languageResult = "C++";
+                    languageReasoning = "A lot of applications in media are written with C++! It's also a very precise language, so you should love it!";
+                  } else if (languageFormatting === "Wild West") {
+                    languageResult = "Javascript";
+                    languageReasoning = "If you work in media, and formatting consistency doesn't bother you, you will love Javascript! Never worry about missing a semi-colon or declaring a global variable again!";
+                  } else {
+                    languageResult = "C++";
+                    languageReasoning = "You work in media but don't seem to have a preference in language formatting. Even if you don't make music, play video games, or exercise, C++ will still be a great option for you as it's one of the most versatile languages out there. Did you know that even Microsoft Windows utilizes C++ for some of its source code?";
+                  }
+                  break;  
           }
+        } else { // if chicken is selected, but no industry is selected
+            languageResult = "";
+            languageReasoning = "";
       }
     } else if (chickenOrEgg === "Egg") {
-      languageResult = "Javascript";
-      languageReasoning = "because you work in tech, but don't care about age. Javascript is applicable to all other survey answers.";
+      languageResult = "All six languages!";
+      languageReasoning = "By selecting egg, you have shown a higher level of intelligence than most, so all languages will come naturally and easily to you (we even discarded all your other answers, as we were so sure on this one!). The egg was first, as the predecessor to the chicken laid the egg that would become the first chicken once the egg hatched.";
       } else { //if chicken or egg isn't answered, then no results
         languageResult = "No recommendation.";
         languageReasoning = "The most important question of all - chicken or egg - was not answered. Try again";
       }
     
     $("#language-suggestion-name").text(languageResult);
+    if (languageResult === "C++") {
+      $("#language-suggestion-img").attr("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png");
+    } else if (languageResult === "Fortran") {
+        $("#language-suggestion-img").attr("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Fortran_logo.svg/1200px-Fortran_logo.svg.png");
+      }
+        else if (languageResult === "C#") {
+          $("#language-suggestion-img").attr("src", "https://pluralsight.imgix.net/paths/path-icons/csharp-e7b8fcd4ce.png");
+        }
+          else if (languageResult === "Python") {
+            $("#language-suggestion-img").attr("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png");
+          }
+            else if (languageResult === "PowerShell") {
+              $("#language-suggestion-img").attr("src", "https://www.saotn.org/wp-content/uploads/2016/08/PowerShell_5.0_logo.png");
+            }
+              else if (languageResult === "Javascript") {
+                $("#language-suggestion-img").attr("src", "https://seeklogo.com/images/J/javascript-logo-8892AEFCAC-seeklogo.com.png");
+              } 
+                else {
+                  $("#language-suggestion-img").attr("src", ""); //set to no photo if no match is found
+                }
+
+
     $("#language-suggestion-reason").text(languageReasoning);
 
 
