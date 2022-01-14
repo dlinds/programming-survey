ebac7d49fc97f396b4982d1877e92e207c19a7da
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-01-14 10:10:10 -0800

    add show/hide of divs when submitting form

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,12 @@ $(document).ready(function () {
   $("form#answers").submit(function (event) {
     event.preventDefault();
 
+    $("#form-questions").hide();
+
+    
+
+
+    $("#form-results").toggle();
     
 
   });
@@ -9,4 +15,10 @@ $(document).ready(function () {
   $("input#email").click(function () {
     $("input#email").val("");
   });
+
+  $("#spin-again-button").click(function () {
+    $("#form-questions").show();
+    $("#form-results").toggle();
+  });
+
 });
